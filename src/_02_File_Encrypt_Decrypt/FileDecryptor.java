81cb8dc91afbe6483c569125dd7e0af030a7bf91
league <league@imac-14.attlocal.net>
2019-10-06 11:31:25 -0700

    work on decrypting

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,6 +1,34 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileDecryptor {
 	// Create a program that opens the file created by FileEncryptor and display
 	// the decrypted message to the user in a JOptionPane.
+	
+	public static void main(String[] args) {
+		String key = "zyxwvutsrqponmlkjihefgdcba";
+		String out = "";
+		File file = new File("src/_02_File_Encrypt_Decrypt");
+		try {
+			FileReader fr = new FileReader(file);
+			char c = (char) fr.read();
+			while(c!=-1) {
+			
+			c=(char) fr.read();
+			
+			
+			
+			
+			
+			}
+			fr.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,7 +1,39 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	// Create a program that takes a message from the user.
 	// Use the methods in the String and Character classes to save
 	// an encrypted form of the message to a file
+	
+	public static void main(String[] args) {
+		String key = "zyxwvutsrqponmlkjihefgdcba";
+		String word = JOptionPane.showInputDialog("Give me a message to encrypt");
+		String out = "";
+		File file = new File("src/_02_File_Encrypt_Decrypt");
+		try {
+			FileWriter fw = new FileWriter(file);
+			
+			for(int i = 0;i<word.length();i++) {
+				for(int k = 0;k<key.length();k++) {
+					if(word.substring(i,i+1).equals(key.substring(k, k+1))) {
+						out+=key.substring(k,k+1);
+					}
+				}
+			}
+			
+			fw.write(out);
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
